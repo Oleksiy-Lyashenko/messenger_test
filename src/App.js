5191fc24d6919ace435f64db52de9ae7da74e5cd
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2022-08-25 21:12:22 +0300

    created all app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,73 @@
+import { useEffect, useState } from 'react';
 import { BsCheckCircle } from 'react-icons/bs';
 import { IoIosContact } from 'react-icons/io';
 import { RiSendPlane2Fill } from 'react-icons/ri';
-import Chat from './components/Chat';
+import ChatList from './components/ChatsList';
+import data from './components/data/data.json';
+import axios from 'axios';
 
 function App() {
+  const [newData, setNewData] = useState(JSON.parse(localStorage.getItem('data')) || []);
+  const [searchChat, setSearchChat] = useState('');
+  const [chosenChat, setChosenChat] = useState(null);
+  const [newText, setNewText] = useState('');
+
+  useEffect(() => {
+    const localData = localStorage.getItem('data');
+
+    if (!localData) {
+      setNewData(data);
+    }
+  }, []);
+
+  const getMessage = async () => {
+    try {
+      const response = await axios.get('https://api.chucknorris.io/jokes/random');
+
+      const newMessage = {
+        id: chosenChat.messages.length,
+        type: 'inbox',
+        text: response.data.value,
+      };
+
+      const newArr = newData.filter((chat) => chat.id !== chosenChat.id);
+
+      const chat = newData.find((chat) => chat.id === chosenChat.id);
+
+      chat.messages.push(newMessage);
+
+      setNewData([chat, ...newArr]);
+
+      localStorage.setItem('data', JSON.stringify([chat, ...newArr]));
+
+
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const sendMessage = () => {
+    const newMessage = {
+      id: chosenChat.messages.length,
+      type: 'outbox',
+      text: newText,
+    };
+
+    const newArr = newData.filter((chat) => chat.id !== chosenChat.id);
+
+    const chat = newData.find((chat) => chat.id === chosenChat.id);
+
+    chat.messages.push(newMessage);
+
+    setNewData([chat, ...newArr]);
+
+    localStorage.setItem('data', JSON.stringify([chat, ...newArr]));
+
+    setNewText('');
+
+    getMessage();
+  };
+
   return (
     <div className="app">
       <div className="container">
@@ -16,52 +80,76 @@ function App() {
               </div>
             </div>
             <div className="search">
-              {/* <input type="text" className='search-style'/> */}
-              <input className="search-style" placeholder="Search or start new chat" />
+              <input
+                className="search-style"
+                placeholder="Search or start new chat"
+                value={searchChat}
+                onChange={(e) => setSearchChat(e.target.value)}
+              />
             </div>
             <div className="chats">
               <h2 className="chats__title">Chats</h2>
               <ul className="chats__list">
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
-                <Chat />
+                {newData
+                  .filter((chat) => {
+                    if (searchChat === '') {
+                      return chat;
+                    } else if (chat.name.toLowerCase().includes(searchChat.toLocaleLowerCase())) {
+                      return chat;
+                    }
+                  })
+                  .map((chat) => (
+                    <div key={chat.id} onClick={() => setChosenChat(chat)}>
+                      <ChatList chat={chat} key={chat.id} />
+                    </div>
+                  ))}
               </ul>
             </div>
           </div>
 
-          <div className="right-side">
-            <div className="user">
-              <div className="img img--user">
-                <IoIosContact className="icon" color="gray" />
-                <BsCheckCircle color="#009c48" className="check" />
-              </div>
-              <h4 className="user__name">Josephina</h4>
-            </div>
-            <div className="text">
-              <div className="text__myself">
-                <div className="img">
+          {chosenChat ? (
+            <div className="right-side">
+              <div className="user">
+                <div className="img img--user">
                   <IoIosContact className="icon" color="gray" />
+                  <BsCheckCircle color="#009c48" className="check" />
                 </div>
-                <div className="text__myself__message">hello me friend </div>
+                <h4 className="user__name">{chosenChat.name}</h4>
               </div>
-              <div className="text__mate">
-                <div className="text__mate__message">hello me friend </div>
+              <div className="text">
+                {chosenChat.messages.map((message) =>
+                  message.type === 'outbox' ? (
+                    <div className="text__myself" key={message.id}>
+                      <div className="img">
+                        <IoIosContact className="icon" color="gray" />
+                      </div>
+                      <div className="text__myself__message">{message.text}</div>
+                    </div>
+                  ) : (
+                    <div className="text__mate" key={message.id}>
+                      <div className="text__mate__message">{message.text}</div>
+                    </div>
+                  ),
+                )}
+              </div>
+              <div className="input">
+                <input
+                  type="text"
+                  className="input__field"
+                  placeholder="Type your message"
+                  value={newText}
+                  onChange={(e) => setNewText(e.target.value)}
+                />
+                <button className="input__button" onClick={() => sendMessage()}>
+                  <RiSendPlane2Fill size="17px" color="#5c5c5c" />
+                </button>
               </div>
             </div>
-            <div className="input">
-              <input type="text" className="input__field" placeholder="Type your message" />
-              <button className="input__button">
-                <RiSendPlane2Fill size="17px" color="#5c5c5c" />
-              </button>
+          ) : (
+            <div className="right-side right-side--no-chat">
+              <p className="no-chat">No chat</p>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
--- src/components/ChatsList.jsx
+++ src/components/ChatsList.jsx
@@ -5,7 +5,9 @@ let options = { month: 'short', day: 'numeric', year: 'numeric' };
 
 const date = new Date().toLocaleDateString('en-US', options);
 
-export default function Chat() {
+export default function ChatList({ chat }) {
+  const { name, messages } = chat;
+
   return (
     <li className="chat">
       <div className="chat__block">
@@ -14,8 +16,12 @@ export default function Chat() {
           <BsCheckCircle color="#009c48" className="check" />
         </div>
         <div className="">
-          <div>Alisa Frolova</div>
-          <div className="chat__message">Last message!</div>
+          <div>{name}</div>
+          <div className="chat__message">
+            {messages[messages.length - 1].text.length > 20
+              ? messages[messages.length - 1].text.slice(0, 20) + '...'
+              : messages[messages.length - 1].text}
+          </div>
         </div>
       </div>
       <div className="chat__time">{date}</div>
